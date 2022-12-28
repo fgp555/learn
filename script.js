af7d3f715db078db920c42f03135193f08167883
Frank GP <fgp555@gmail.com>
2022-12-28 15:23:28 -0500

    modify fgp

--- script.js
+++ script.js
@@ -1,87 +1,77 @@
 // unordered list
-const list = [
-    'list item a',
-    'list item b',
-    'list item c',
-    'list item d',
-    'list item e',
-    'list item a',
-    'list item b',
-    'list item c',
-    'list item d',
-    'list item e',
-];
+const list = ["list item 1", "list item 2", "list item 3", "list item 4", "list item 5", "list item 6", "list item 7", "list item 8", "list item 9", "list item 0"];
 
-const ul = document.querySelector('.pags ul');
-const nav = document.querySelector('.pags nav');
+const ul = document.querySelector(".pags ul");
+const nav = document.querySelector(".pags nav");
 
 let pag = 1;
-let rows = 4;
+let rows = 3;
 
 // ========== function 1 DisplayList... ==========
-function DisplayList(list_1, ul_1, rows_1, pag_1) {
-    ul_1.innerHTML = '';
-    pag_1--;
+function DisplayList(list_1, pag_1) {
+  ul.innerHTML = "";
+  pag_1--;
 
-    let start = rows_1 * pag_1; // 2 * 0 = 0
-    let end = start + rows_1; // 0 + 2 = 2
+  let start = rows * pag_1; // 2 * 0 = 0
+  let end = start + rows; // 0 + 2 = 2
 
-    let listSlice = list_1.slice(start, end);
-    
-    for (let i = 0; i < listSlice.length; i++) {
-        let item = listSlice[i];
-        // console.log(list_1[i])
+  let listSlice = list_1.slice(start, end);
 
-        let createLi = document.createElement('li');
-        createLi.innerText = item;
+  for (let i = 0; i < listSlice.length; i++) {
+    let item = listSlice[i];
+    // console.log(list_1[i])
 
-        ul_1.appendChild(createLi);
-    }
+    let createLi = document.createElement("li");
+    createLi.innerText = item;
+
+    ul.appendChild(createLi);
+  }
 }
-DisplayList(list, ul, rows, pag)
+DisplayList(list, pag);
 // ========== function 1 DisplayList. ==========
 
 // ========== functio 2 CreatePags... ==========
-function CreatePags(list_2, nav_2, rows_2) {
-    nav_2.innerHTML = '';
+function CreatePags() {
+  nav.innerHTML = "";
 
-    let list_rows = list_2.length / rows_2;
-    let mathCeil = list_rows;
+  let list_rows = list.length / rows;
+  //   console.log(list_rows);
+  //   let mathCeil = Math.ceil(list_rows);
+  let mathCeil = list_rows;
+  //   console.log(mathCeil);
 
-    for (let i = 1; i < mathCeil + 1; i++){
-        // console.log(i);
-        let btn = DisplayBtn(i, list_2);
-        nav_2.appendChild(btn)
-    }
+  for (let i = 1; i < mathCeil + 1; i++) {
+    // console.log(i);
+    let btn = DisplayBtn(i);
+    nav.appendChild(btn);
+  }
 }
-CreatePags(list, nav, rows)
+CreatePags();
 // ========== functio 2 CreatePags. ==========
 
-// 
+//
 
 // ========== function 3 DisplayBtn... ==========
-function DisplayBtn(pageNum, items) {
-    let createBtn = document.createElement('button');
-    createBtn.innerText = pageNum;
+function DisplayBtn(pageNum) {
+  let createBtn = document.createElement("button");
+  createBtn.innerText = pageNum;
 
-    if (pag == pageNum) createBtn.classList.add('active');
+  if (pag == pageNum) createBtn.classList.add("active");
 
-    // ========== function 4 ClickBtn...  ==========
-    createBtn.addEventListener('click', function () {
-        pag = pageNum;
-        DisplayList(items, ul, rows, pag)
+  // ========== function 4 ClickBtn...  ==========
+  createBtn.addEventListener("click", function () {
+    pag = pageNum;
+    DisplayList(list, pag);
 
-        // classList...
-        let btnActive = document.querySelector('.pags button.active');
-        btnActive.classList.remove('active');
+    // classList...
+    let btnActive = document.querySelector(".pags button.active");
+    btnActive.classList.remove("active");
 
-        createBtn.classList.add("active")
-        // classList.
-    })
-    // ========== function 4 ClickBtn.  ==========
-    
+    createBtn.classList.add("active");
+    // classList.
+  });
+  // ========== function 4 ClickBtn.  ==========
 
-    return createBtn;
+  return createBtn;
 }
 // ========== function 3 DisplayBtn. ==========
-
